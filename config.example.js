ba4d304e1cdd8ee808a9564833410aff005a810b
Jack Fan <jackfan@invisibi.com>
2016-12-25 11:55:06 +0800

    Really sort all files from different class by ‘updatedAt’ (It was sorted by class, then by ‘updatedAt’)

--- config.example.js
+++ config.example.js
@@ -3,6 +3,10 @@ var S3Adapter = require('parse-server-s3-adapter');
 var GCSAdapter = require('parse-server-gcs-adapter');
 
 module.exports = {
+  updatedAt_greaterThan: "2016-12-24T10:06:23.412Z", // 2016-12-23T13:06:37.853Z
+  updatedAt_greaterThan_autoUpdateWhenComplete: true,
+  //updatedAt_lessThan: "2016-11-29T10:00:00.001Z",
+
   applicationId: "PARSE_APPLICATION_ID",
   masterKey: "PARSE_MASTER_KEY",
   mongoURL: "mongodb://<username>:<password>@mongourl.com:27017/database_name",
@@ -25,6 +29,7 @@ module.exports = {
   gcs_keyFilename: "credentials.json",
   gcs_bucket: "BUCKET_NAME",
 
+  asyncLimit: 10
   // Or set filesAdapter to a Parse Server file adapter
   // filesAdapter: new FileAdapter({
   //  filesSubDirectory: './downloaded_files'
